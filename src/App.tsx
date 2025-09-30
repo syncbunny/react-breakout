0100596e2e3c7bfeb1c9b1a384e5b466d27eeec1
syncbunny <syncbunny@gmail.com>
2025-09-30 10:43:04 +0900

    ERROR

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+    <div>error1</div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
